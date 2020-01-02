d71bf815505e949b0c7400e997861efdb1ec0a6c
Luis Sanchez <lsancheza@vectoritcgroup.com>
2020-01-02 18:53:37 +0100

    Terminado funnel a espera de información

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -5,3 +5,42 @@
 
 
 import '../scss/main_index.scss';
+
+/* 
+const _array =[
+    {id: 0, ciudad:"Madrid"},
+    {id: 1, ciudad:"Barcelona"},
+    {id: 2, ciudad:"Budapest"},
+    {id: 3, ciudad:"Valencia"},
+    {id: 4, ciudad:"Merida"},
+    {id: 5, ciudad:"Salamanca"}
+
+];
+
+function recup(busq) {
+    const result=_array.find((data)=>{
+        return data.id===busq
+    })
+
+    return result.ciudad
+}
+
+let result=recup(2)
+
+console.log(result);
+
+
+let elem=document.querySelector(".camb")
+let string=elem.textContent;
+
+console.log(string)
+
+let _result=string.replace(/saca/g,(data)=>{
+    return  data=`
+    <span class="resalta">${data}</span>
+    `
+   
+});
+console.log(_result)
+
+elem.innerHTML=_result; */
